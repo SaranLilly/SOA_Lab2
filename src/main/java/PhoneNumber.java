e1856a2c526b7bef81bb8382feea18e16b3aad2b
sarun <sarun@MSI>
2023-12-12 19:19:41 +0700

    SOA_Lab2

---
+++ src/main/java/PhoneNumber.java
@@ -0,0 +1,22 @@
+
+public class PhoneNumber {
+private String number;
+	
+	public PhoneNumber() {
+		
+	}
+	
+	public PhoneNumber(String number) {
+		this.number = number;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+//	public void setNumber() {
+//		this.number = number;
+//	}
+}
